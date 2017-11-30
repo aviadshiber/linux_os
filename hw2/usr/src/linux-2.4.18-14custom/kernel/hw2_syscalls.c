ca78df47d1c8011718debfbefbd5c1e7400a9074
alexbla2 <alexblago3@gmail.com>
2017-11-30 18:41:58 +0200

    updated (functions moved to sched.c)

--- hw2/usr/src/linux-2.4.18-14custom/kernel/hw2_syscalls.c
+++ hw2/usr/src/linux-2.4.18-14custom/kernel/hw2_syscalls.c
@@ -3,25 +3,6 @@
 #include <linux/slab.h>
 #include <asm/uaccess.h>
 
-// task_t* found_task=find_task_by_pid(pid);
-int sys_get_remaining_timeslice(pid_t pid){
-    //printk("\n sys_get_remaining_timeslice called \n");
-    if(pid<0){
-        return -ESRCH;
-    }
-    task_t* found_task=find_task_by_pid(pid);
-    if(!found_task){
-        return -ESRCH;
-    }
-    if(SCHED_FIFO == found_task->policy ){
-        return -EINVAL;
-    }
-    //should we return zero timeslice on zombie?
-    if(found_task->state==TASK_ZOMBIE){
-        return 0;
-    }
-    return found_task->time_slice;
-}
 
 int sys_get_total_processor_usage(pid_t pid){
     //printk("\n sys_get_total_processor_usage called \n");  
@@ -50,27 +31,6 @@ int sys_get_total_time_in_runqueue(pid_t pid){
         return -ESRCH;
     }
     return found_task->total_time_in_runqueue;
-}
-
-int sys_sacrifice_timeslice(pid_t pid){
-    //printk("\n sys_sacrifice_timeslice called \n");
-    if(pid<0){
-        return -ESRCH;
-    }
-    task_t* found_task=find_task_by_pid(pid);
-    if(!found_task){
-        return -ESRCH;
-    }
-    if( (pid == current->pid) || (found_task->policy == SCHED_FIFO) || (found_task->state == TASK_ZOMBIE)  ){
-        return -EINVAL;
-    }
-    if( current->policy == SCHED_FIFO ){
-        return -EPERM;
-    }
-    int currentTimeSlice=current->time_slice;
-    current->sacrafice=1; //the current_time slice will be nullified in tick according to this flag
-    found_task->time_slice+=currentTimeSlice;
-    return currentTimeSlice;
 }
 
 
